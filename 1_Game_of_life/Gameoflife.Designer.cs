99366aa752c2b6f798705aea85998523270b275a
ogama <ogama@DESKTOP-82PHLEE>
2024-03-31 23:30:11 +0300

    Add project files.

---
+++ 1_Game_of_life/Gameoflife.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace _1_Game_of_life
+{
+    partial class game
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.start_button = new System.Windows.Forms.Button();
+            this.stop_button = new System.Windows.Forms.Button();
+            this.Bluecolor = new System.Windows.Forms.Button();
+            this.Redcolor = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // start_button
+            // 
+            this.start_button.BackColor = System.Drawing.Color.LightPink;
+            this.start_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.841584F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.start_button.Location = new System.Drawing.Point(797, 409);
+            this.start_button.Name = "start_button";
+            this.start_button.Size = new System.Drawing.Size(75, 60);
+            this.start_button.TabIndex = 4;
+            this.start_button.Text = "Начать";
+            this.start_button.UseVisualStyleBackColor = false;
+            this.start_button.Click += new System.EventHandler(this.start_button_Click_1);
+            // 
+            // stop_button
+            // 
+            this.stop_button.BackColor = System.Drawing.Color.LightPink;
+            this.stop_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.841584F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.stop_button.Location = new System.Drawing.Point(797, 488);
+            this.stop_button.Name = "stop_button";
+            this.stop_button.Size = new System.Drawing.Size(75, 60);
+            this.stop_button.TabIndex = 5;
+            this.stop_button.Text = "Стоп";
+            this.stop_button.UseVisualStyleBackColor = false;
+            this.stop_button.Click += new System.EventHandler(this.stop_button_Click);
+            // 
+            // Bluecolor
+            // 
+            this.Bluecolor.BackColor = System.Drawing.Color.SlateBlue;
+            this.Bluecolor.Location = new System.Drawing.Point(797, 101);
+            this.Bluecolor.Margin = new System.Windows.Forms.Padding(2);
+            this.Bluecolor.Name = "Bluecolor";
+            this.Bluecolor.Size = new System.Drawing.Size(80, 40);
+            this.Bluecolor.TabIndex = 6;
+            this.Bluecolor.UseVisualStyleBackColor = false;
+            this.Bluecolor.Click += new System.EventHandler(this.bluecolor_Click);
+            // 
+            // Redcolor
+            // 
+            this.Redcolor.BackColor = System.Drawing.Color.IndianRed;
+            this.Redcolor.Location = new System.Drawing.Point(797, 41);
+            this.Redcolor.Margin = new System.Windows.Forms.Padding(2);
+            this.Redcolor.Name = "Redcolor";
+            this.Redcolor.Size = new System.Drawing.Size(80, 40);
+            this.Redcolor.TabIndex = 7;
+            this.Redcolor.UseVisualStyleBackColor = false;
+            this.Redcolor.Click += new System.EventHandler(this.redcolor_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoScroll = true;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(779, 536);
+            this.panel1.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(884, 560);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Redcolor);
+            this.Controls.Add(this.Bluecolor);
+            this.Controls.Add(this.start_button);
+            this.Controls.Add(this.stop_button);
+            this.ForeColor = System.Drawing.Color.Crimson;
+            this.Name = "game";
+            this.Text = "Жизнь";
+           
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button start_button;
+        private System.Windows.Forms.Button stop_button;
+        private System.Windows.Forms.Button Bluecolor;
+        private System.Windows.Forms.Button Redcolor;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ 1_Game_of_life/Gameoflife.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1_Game_of_life
+{
+    public partial class game : Form
+    {
+
+        int height= 20; //height wid - размер одной клетки матрицы
+        int widht = 20; //
+        int ch = 0;
+        int h, w; // количество клеток на panel
+        Label[,] lab; // матрица из одной клетки сетки
+        int col = 0; // выбор цвета
+        Random rand = new Random(); //инициализирует новый экземляр Random класс с помощью начального значения по умолчанию, завис. от времени
+
+        public game() 
+        {
+            InitializeComponent(); //метод для поддержки конструктора
+            Game_area(); 
+        }
+        private void New_labels(int a, int c) // установка параметров на сетку в игровой области
+        {
+            lab[a, c] = new Label(); //массив/матрица из клеток
+            lab[a, c].Parent = panel1; //назначает родительский контейнер элемента управлениz
+            lab[a, c].BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle; //задает стиль для границы для эл-та управления (сетки)
+            lab[a, c].BackColor = System.Drawing.Color.White ; //цвет фона в сетке
+            lab[a, c].Location = new Point(a * widht, c * height); //указываем точку с параметрами ширины и высоты
+            lab[a, c].Size = new System.Drawing.Size(widht + 1, height + 1);
+            lab[a, c].Click += new EventHandler(this.label_Click);
+        }
+
+        private void Game_area() //метод создания сетки
+        {
+            w = 35; // количество клеток в строке
+            h = 25;// количество клеток в столбике
+            lab = new Label[w, h]; // инициализация массива
+            for (int i = 0; i < w; i++)
+            {
+                for (int j = 0; j < h; j++)
+                {
+                    New_labels(i, j); // заполнение панели
+                }
+            }
+        }
+        private void bluecolor_Click(object sender, EventArgs e)
+        {
+            col = 1;
+        }
+
+        private void redcolor_Click(object sender, EventArgs e)
+        {
+            col = 2;
+        }
+        
+        private int Blueneighbour(int a, int c) //метод проверки на нахождение синих клеток по соседству
+        {
+           ch = 0;
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    if (i == 0 && j == 0)
+                    {
+                        continue;
+                    }
+                    if (Convert.ToInt32(lab[(a + i + w) % w, (c + j + h) % h].Tag) == 1) 
+                    {
+                        ch++;
+                    }
+                }
+            }
+            return ch;
+        }
+        private int Redneighbour(int a, int c) // метод нахождения красных клеток по соседству
+        {
+          ch = 0;
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    if (i == 0 && j == 0)
+                    {
+                        continue;
+                    }
+                    if (Convert.ToInt32(lab[(a + i + w) % w, (c + j + h) % h].Tag) == 2) // проверка на соседние элементы красного цвета через края включительно
+                    {
+                        ch++;
+                    }
+                }
+            }
+            return ch;
+        }
+
+        private void start_button_Click_1(object sender, EventArgs e)
+        {
+            timer1.Start(); //время жизни начинается с нажатием кнопки старта, запускается таймер
+            panel1.Enabled = false; //доступ к панели по время игры заблокирован
+        }
+
+        private void SearchBW()  //жизнь синих клеток
+        {
+            for (int i = 0; i < w; i++)
+            {
+                for (int j = 0; j < h; j++)
+                {
+                    int sum = Blueneighbour(i, j); // количество элементов синего цвета вокруг клетки
+                    if (sum == 3)
+                    {
+                        lab[i, j].BackColor = Color.Blue;
+                        continue;
+                    }
+                    if ((sum == 2 || sum == 3) && Convert.ToInt32(lab[i, j].Tag) == 1)
+                    {
+                        lab[i, j].BackColor = Color.Blue;
+
+                    }
+                    else
+                    {
+                        if (lab[i, j].BackColor == Color.White|| lab[i, j].BackColor == Color.Red)
+                        {
+                            if (sum == 3)
+                            {
+                                lab[i, j].BackColor = Color.Blue;
+                            }
+                            continue;
+                        }
+                        lab[i, j].BackColor = Color.White;
+                    }
+                }
+            }
+        }
+        private void SearchRW() // поиск красных клеток
+        {
+            for (int i = 0; i < w; i++)
+            {
+                for (int j = 0; j < h; j++)
+                {
+                    int sum = Redneighbour(i, j); // количество красных клеток вокруг нынешней 
+                    if (sum == 3)
+                    {
+                        lab[i, j].BackColor = Color.Red;
+                        continue;
+                    }
+                    if ((sum == 2 || sum == 3) && Convert.ToInt32(lab[i, j].Tag) == 2)
+                    {
+                        lab[i, j].BackColor = Color.Red;
+                    }
+                    else
+                    {
+                        if (lab[i, j].BackColor == Color.Blue)
+                        {
+                            if (sum == 3)
+                            {
+                                lab[i, j].BackColor = Color.Red;
+                            }
+                            continue;
+                        }
+                        lab[i, j].BackColor = Color.White;
+                    }
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int a = rand.Next(1, 3);
+            if (a == 1) //доминируют синие
+            {
+                SearchBW(); // вызов метода для смены цвета синих
+                SearchRW(); // вызов метода для смены цвета красных
+            }
+            if (a == 2) //доминируют красные
+            {
+                SearchRW(); // вызов метода для смены цвета красных
+                SearchBW(); // вызов метода для смены цвета синих
+            }
+
+            for (int i = 0; i < w; i++) //смена тэга
+            {
+                for (int j = 0; j < h; j++)
+                {
+                    if (lab[i, j].BackColor == Color.White)
+                    {
+                        lab[i, j].Tag = 0;
+                    }
+                    if (lab[i, j].BackColor == Color.Blue)
+                    {
+                        lab[i, j].Tag = 1;
+                    }
+                    if (lab[i, j].BackColor == Color.Red)
+                    {
+                        lab[i, j].Tag = 2;
+                    }
+                }
+            }
+        }
+        private void label_Click(object sender, EventArgs e) //работа с закрашиванием красных или синих клеток на панели
+        {
+            int x = ((Label)sender).Location.X / widht; // определение позиции элемента в строке
+            int y = ((Label)sender).Location.Y / height;// определение позиции элемента в столбце
+            if (lab[x, y].BackColor == System.Drawing.Color.Red || lab[x, y].BackColor == System.Drawing.Color.Blue)
+            {
+                lab[x, y].Tag = 0;
+                lab[x, y].BackColor = System.Drawing.Color.White; // возврат на белый цвет закрашенной клетки
+                return;
+            }
+            if (col == 0) // если цвет не выбран
+            {
+                MessageBox.Show("Выберите цвет для жизни."); // сообщение пользователю
+            }
+            if (col == 1) // если выбран синий цвет
+            {
+                lab[x, y].BackColor = System.Drawing.Color.Blue; // закрасить синим
+                lab[x, y].Tag = 1; // смена тэга
+            }
+            if (col == 2) // если выбран красный цвет
+            {
+                lab[x, y].BackColor = System.Drawing.Color.Red; // закрасить красным
+                lab[x, y].Tag = 2; // смена тэга 
+            }
+        }
+
+        private void stop_button_Click(object sender, EventArgs e)
+        {
+            timer1.Stop(); // остановка таймера
+            panel1.Enabled = true; // открытие панели
+
+        }
+    }
+}
